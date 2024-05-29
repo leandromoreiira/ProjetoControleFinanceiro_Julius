da42e474c0823c7aa41ef0646ac79558c4f49d64
Leo Victor <contato.leandrovmoreira@gmail.com>
2024-05-28 21:55:45 -0300

    Atualização dos diretorios

---
+++ ProjetoControleFinanceiro_Julius/FinanceiroJulius.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoControleFinanceiro_Julius {
+    class FinanceiroJulius {
+
+        public static double saldo;
+
+        public static void adicionarDespesa(double valor,string descricao) {
+
+            if(valor>saldo) {
+                Console.WriteLine("Saldo insuficiente para essa despesa! ");
+            }
+            else {
+                saldo=saldo-valor;
+                Console.WriteLine("Despesa adicionada: "+descricao);
+                MostrarSaldo();
+            }
+        }
+
+        public static void adicionarEconomia(double valor,string descricao) {
+            saldo=saldo+valor;
+            Console.WriteLine("Economia adicionada"+descricao);
+            MostrarSaldo();
+        }
+
+        public static void MostrarSaldo() {
+            Console.WriteLine("Seu saldo é: "+saldo.ToString("F2",CultureInfo.InvariantCulture));
+        }
+
+
+        public static void DefinirSaldoInicial(double valor) {
+            saldo=valor;
+            MostrarSaldo();
+        }
+
+    }
+}
---
+++ ProjetoControleFinanceiro_Julius/Program.cs
@@ -0,0 +1,36 @@
+﻿using ProjetoControleFinanceiro_Julius;
+using System.Globalization;
+
+int EscolhaUsuario;
+string DataAtual;
+
+
+
+
+//Definição e exibição do saldo atual.
+Console.WriteLine("Olá Julius! Informe seu saldo atual: ");
+double SaldoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+FinanceiroJulius.DefinirSaldoInicial(SaldoInicial);
+
+
+//Adicionando despesas e mostrando novo saldo
+
+Console.WriteLine("Olá Julius, Informe o valor da sua despesa R$: ");
+double valorGasto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+Console.WriteLine("qual foi o motivo: ");
+string descricao = Console.ReadLine();
+FinanceiroJulius.adicionarDespesa(valorGasto,descricao);
+
+
+//Adicionando economia e mostrando novo saldo
+
+Console.WriteLine("Olá Julius ,Como você fez essa economia ");
+double ValorEconomizado = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+Console.WriteLine("qual foi o motivo: ");
+string descricaoEconomia = Console.ReadLine();
+FinanceiroJulius.adicionarEconomia(ValorEconomizado,descricaoEconomia);
+
+
+
+
+
